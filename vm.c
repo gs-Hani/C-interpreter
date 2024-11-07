528beff23cead7ec026de993371f2aae0da2a339
gh-Hani <hani_alazawi@hotmail.com>
2024-11-07 07:43:38 +0400

    end of chapter 16.1.1

--- vm.c
+++ vm.c
@@ -110,9 +110,7 @@ static InterpretResult run() {
 	#undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  vm.lc = vm.chunk->const_long;
-  return run();
+InterpretResult interpret(const char* source) {
+	compile(source);
+	return INTERPRET_OK;
 }
